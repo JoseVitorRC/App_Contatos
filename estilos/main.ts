9c531a5a5336d348f1f8123704a95e17798b13c8
José Vítor <cavalcanti.josevitor@gmail.com>
2025-03-22 01:09:35 +0000

    Segundo commit App Contatos

---
+++ estilos/main.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 20 },
+    input: {
+      borderWidth: 1,
+      borderColor: "#ccc",
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10,
+    },
+  });
+  
+export default styles;  
